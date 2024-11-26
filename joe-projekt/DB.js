7f75ad8acd73f74af9396222eb7892d7232ba914
Amaan9621 <167350883+Amaan9621@users.noreply.github.com>
2024-11-26 13:08:32 +0100

    deeddede

---
+++ joe-projekt/DB.js
@@ -0,0 +1,11 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./db.sqlite");
+
+// Opretter en tabel som indeholder primærnøgle id, url, tidspunkt og caption
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE if not exists uploads (phone_number TEXT PRIMARY KEY, password TEXT NOT NULL, email TEXT NOT NULL, name TEXT NOT NULL, loyalty_counter INTEGER DEFAULT 0, datetime integer)"
+  );
+});
+
