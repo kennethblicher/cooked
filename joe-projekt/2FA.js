00cf2e2a60afc1a2b87600b39c4f2ecc1942aff6
TigerFaraz <faah23ac@student.cbs.dk>
2024-11-27 11:16:34 +0100

    2 FA, MEN ALLE HTML LINKS ER COOKED
    2 FA, MEN ALLE HTML LINKS ER COOKED

--- joe-projekt/2FA.js
+++ joe-projekt/2FA.js
@@ -5,16 +5,24 @@ const accountSid = "AC789d4a67dd55c1d86d4a4141cd240361";
 const authToken = "e3638f3c7f71d76b4a811011e0b45214";
 const client = twilio(accountSid, authToken);
 
-async function createText(modtager, besked) {
+async function createText(modtager) {
+
+  const digits = [];
+  for (let i = 0; i < 5; i++) {
+    const randomDigit = Math.floor(Math.random() * 10) + 1; // Genererer 6 tilfældige tal
+    digits.push(randomDigit);
+  }
+  const finalDigits = digits.join(""); // Samler de 6 tal til en string
   const message = await client.messages.create({
     from: "+18504006662",
     to: modtager,
-    body: besked,
+    body: `Hej! Din bekræftelseskode er ${finalDigits}`,
   });
 
   console.log(message);
 }
-const test = document.getElementById("signupCreate").value;
-console.log(test)
+const tlfNumber = document.getElementById("signupCreate").value;
+console.log(tlfNumber)
 
-createText(test, "Hej! Det er fra Joe, velkommen til");
+
+createText(tlfNumber);
